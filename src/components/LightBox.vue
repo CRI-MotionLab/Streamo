ad5711f73531482f02ca86f798f1fcbaf9e19c92
Joseph <joseph.larralde@gmail.com>
2018-10-29 17:34:35 +0100

    added welcome lightbox to force user interaction

---
+++ src/components/LightBox.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="lightbox" :class="showLightBox ? 'on' : ''">
+    <div id="lightbox-contents">
+      <img id="movuino-icon">
+      <div>
+        <button v-on:click="onClick">
+          GO
+        </button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        showLightBox: true,
+      };
+    },
+    methods: {
+      onClick() {
+        this.showLightBox = false;
+      },
+    },
+  };
+</script>
