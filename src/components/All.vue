0dbad8b25c68451f1812c928d8302975e8195688
Joseph <joseph.larralde@gmail.com>
2019-03-11 14:05:42 +0100

    improved planet visualization, added app icon

--- src/components/All.vue
+++ src/components/All.vue
@@ -50,6 +50,11 @@ https://keithclark.co.uk/articles/creating-3d-worlds-with-html-and-css/
 
   const oneOverG = 1 / 9.8;
   const oneOverThreeSixty = 1 / 360;
+  const radToDeg = 180 / Math.PI;
+
+  const cubeSide = 100; // pixels
+  const ringRadius = 150; // pixels
+  const satRadius = 25; // pixels
 
   function createFace(w, h, x, y, z, rx, ry, rz, color) {
     var face = document.createElement('div');
@@ -68,43 +73,53 @@ https://keithclark.co.uk/articles/creating-3d-worlds-with-html-and-css/
     return face;
   }
 
-  function createRing() {
+  function createRing(thickness, color) {
     var ring = document.createElement('div');
     ring.className = 'threedee ring';
     ring.style.cssText = `
-      background: url('./assets/bluering.png');
-      background-size: 300px;
-      width: 300px;
-      height: 300px;
-      margin-top: -150px;
-      margin-left: -150px;
+      width: ${2 * ringRadius}px;
+      height: ${2 * ringRadius}px;
+      margin-top: -${ringRadius}px;
+      margin-left: -${ringRadius}px;
+      border-radius: ${ringRadius}px;
+      border: ${thickness}px solid ${color};
       transform-style: preserve-3d;
-      transform:
-      translate3d(0, 0, 0)
-      rotateX(${Math.PI / 2}rad);
     `;
 
-    return ring;    
+    return ring;        
   }
 
-  function createSatellite(rx, ry, rz, x, y, z, color) { // color should be 'red', 'blue' or 'yellow'
+  function createSatellite(color) {
     var sat = document.createElement('div');
     sat.className = 'threedee satellite';
     sat.style.cssText = `
-      background: url('./assets/satellite-${color}.png');
-      background-size: 50px;
-      width: 50px;
-      height: 50px;
-      margin-top: -25px;
-      margin-left: -25px;
+      width: ${2 * satRadius}px;
+      height: ${2 * satRadius}px;
+      margin-top: -${satRadius}px;
+      margin-left: -${satRadius}px;
+      border-radius: ${satRadius}px;
+      background-color: ${color};
       transform-style: preserve-3d;
-      transform:
-      rotateX(${rx.toFixed(2)}rad) rotateY(${ry.toFixed(2)}rad) rotateY(${rz.toFixed(2)}rad)
-      translateX(${x.toFixed(2)}px) translateY(${y.toFixed(2)}px) translateZ(${z.toFixed(2)}px);
     `;
 
     return sat;
   }
+
+  // function createSatellite(color) {
+  //   var sat = document.createElement('div');
+  //   sat.className = 'threedee satellite';
+  //   sat.style.cssText = `
+  //     background: url('./assets/satellite-${color}.png');
+  //     background-size: ${2 * satRadius}px;
+  //     width: ${2 * satRadius}px;
+  //     height: ${2 * satRadius}px;
+  //     margin-top: -${satRadius}px;
+  //     margin-left: -${satRadius}px;
+  //     transform-style: preserve-3d;
+  //   `;
+
+  //   return sat;
+  // }
 
   export default {
     name: 'all',
@@ -116,53 +131,80 @@ https://keithclark.co.uk/articles/creating-3d-worlds-with-html-and-css/
     mounted() {
       this.filter = [];
       for (let i = 0; i < 2; i++) {
-        this.filter.push([0, 0, 0, 0, 0, 0]);
+        this.filter.push([0, 0, 0, 0, 0, 0, 0, 0, 0]);
       }
 
       const side = 100;
-      const halfSide = side * 0.5;
+      const halfSide = cubeSide * 0.5;
       const all = document.querySelector('#planet-cube');
+
       const cube = document.createElement('div');
       cube.className = 'threedee';
       cube.style.cssText = `
         transform-style: preserve-3d;
       `;
-      cube.appendChild(createFace(side, side, 0, 0, halfSide, 0, 0, 0, lightYellow)); // front
-      cube.appendChild(createFace(side, side, 0, 0, -halfSide, 0, 0, Math.PI, lightYellow)); // back
-      cube.appendChild(createFace(side, side, -halfSide, 0, 0, 0, -Math.PI / 2, 0, lightBlue)); // left
-      cube.appendChild(createFace(side, side, halfSide, 0, 0, 0, Math.PI / 2, 0, lightBlue)); // right
-      cube.appendChild(createFace(side, side, 0, -halfSide, 0, Math.PI / 2, 0, 0, lightRed)); // bottom
-      cube.appendChild(createFace(side, side, 0, halfSide, 0, -Math.PI / 2, 0, 0, lightRed)); // top
+      cube.appendChild(createFace(cubeSide, cubeSide, 0, 0, halfSide, 0, 0, 0, lightYellow)); // front
+      cube.appendChild(createFace(cubeSide, cubeSide, 0, 0, -halfSide, 0, 0, Math.PI, lightYellow)); // back
+      cube.appendChild(createFace(cubeSide, cubeSide, -halfSide, 0, 0, 0, -Math.PI / 2, 0, lightBlue)); // left
+      cube.appendChild(createFace(cubeSide, cubeSide, halfSide, 0, 0, 0, Math.PI / 2, 0, lightBlue)); // right
+      cube.appendChild(createFace(cubeSide, cubeSide, 0, -halfSide, 0, Math.PI / 2, 0, 0, lightRed)); // bottom
+      cube.appendChild(createFace(cubeSide, cubeSide, 0, halfSide, 0, -Math.PI / 2, 0, 0, lightRed)); // top
 
-      const ring = createRing();
-      cube.appendChild(ring);
       all.appendChild(cube);
 
-      const radius = 200;
-      const satellites = document.createElement('div');
-      satellites.className = 'threedee';
-      satellites.style.cssText = `
-        transform-style: preserve-3d;
-      `;
-      satellites.appendChild(createSatellite(0, 0, 0, -radius, 0, 0, 'yellow'));
-      satellites.appendChild(createSatellite(0, 0, 0, radius, 0, 0, 'yellow'));
-      satellites.appendChild(createSatellite(0, 0, 0, 0, -radius, 0, 'blue'));
-      satellites.appendChild(createSatellite(0, 0, 0, 0, radius, 0, 'blue'));
-      satellites.appendChild(createSatellite(0, 0, 0, 0, 0, -radius, 'red'));
-      satellites.appendChild(createSatellite(0, 0, 0, 0, 0, radius, 'red'));
-      all.appendChild(satellites);
+      const ring = createRing(5, darkBlue);
+      all.appendChild(ring);
+
+      const satellites = [
+        createSatellite(lightYellow),
+        createSatellite(lightYellow),       
+        createSatellite(lightBlue),
+        createSatellite(lightBlue),
+        createSatellite(lightRed),
+        createSatellite(lightRed),
+      ];
+
+      const satelliteTranslations = [
+        [ -ringRadius, 0, 0 ],
+        [ ringRadius, 0, 0 ],
+        [ 0, -ringRadius, 0 ],
+        [ 0, ringRadius, 0 ],
+        [ 0, 0, -ringRadius ],
+        [ 0, 0, ringRadius ],
+      ];
+
+      for (let i = 0; i < satellites.length; i++) {
+        all.appendChild(satellites[i]);
+      }
+
+      ////////// ANIMATION CALLBACK :
 
       this.intervalId = setInterval(() => {
-        const values = this.$store.state.accGyrValues;
+        const accGyrValues = this.$store.state.accGyrValues;
+        const magValues = this.$store.state.normalizedMagValues;
+
+        let theta = 0;
+        if (magValues.x !== 0 && magValues.y !== 0) {
+          theta = Math.acos(magValues.x / Math.sqrt(Math.pow(magValues.x, 2) +
+                                          Math.pow(magValues.y, 2)));
+          if (magValues.y < 0) {
+            theta = 2* Math.PI - theta;
+          }
+        }
+
+        let phi = Math.acos(magValues.z);
+
         const frame = [ 
-          values.x, values.y, values.z,
-          values.alpha, values.beta, values.gamma
+          accGyrValues.x, accGyrValues.y, accGyrValues.z,
+          accGyrValues.alpha, accGyrValues.beta, accGyrValues.gamma,
+          0, phi, -theta,
         ];
+
         this.filter.push(frame);
         this.filter.splice(0, 1);
 
         let filtered = [];
-        for (let i = 0; i < 6; i++) {
+        for (let i = 0; i < 9; i++) {
           let sum = 0;
           for (let j = 0; j < this.filter.length; j++) {
             sum += this.filter[j][i];
@@ -179,16 +221,32 @@ https://keithclark.co.uk/articles/creating-3d-worlds-with-html-and-css/
           rotateZ(${(filtered[5] * 0.1).toFixed(2)}deg)
         `;
 
-        // ring.style.transform = ``;
-
-        satellites.style.transform = `
-          rotateX(${(filtered[3] * -0.1).toFixed(2)}deg)
-          rotateY(${(filtered[4] * -0.1).toFixed(2)}deg)
-          rotateZ(${(filtered[5] * -0.1).toFixed(2)}deg)
+        ring.style.transform = `
           translateX(${(filtered[0] * -10).toFixed(2)}px)
           translateY(${(filtered[1] * 20).toFixed(2)}px)
           translateZ(${(filtered[2] * -10 - 100).toFixed(2)}px)
+          rotateX(${(filtered[6] * 1).toFixed(2)}rad)
+          rotateY(${(filtered[7] * 1).toFixed(2)}rad)
+          rotateZ(${(filtered[8] * 1).toFixed(2)}rad)
+          rotateX(${(Math.PI / 2).toFixed(2)}rad)
         `;
+
+        for (let i = 0; i < satellites.length; i++) {
+          satellites[i].style.transform = `
+            translateX(${(filtered[0] * -10).toFixed(2)}px)
+            translateY(${(filtered[1] * 20).toFixed(2)}px)
+            translateZ(${(filtered[2] * -10 - 100).toFixed(2)}px)
+            rotateX(${(filtered[6] * 1).toFixed(2)}rad)
+            rotateY(${(filtered[7] * 1).toFixed(2)}rad)
+            rotateZ(${(filtered[8] * 1).toFixed(2)}rad)
+            translateX(${satelliteTranslations[i][0].toFixed(2)}px)
+            translateY(${satelliteTranslations[i][1].toFixed(2)}px)
+            translateZ(${satelliteTranslations[i][2].toFixed(2)}px)
+            rotateZ(${(filtered[8] * -1).toFixed(2)}rad)
+            rotateY(${(filtered[7] * -1).toFixed(2)}rad)
+            rotateX(${(filtered[6] * -1).toFixed(2)}rad)
+          `;
+        }
       }, 50);
     },
     beforeDestroy() {
